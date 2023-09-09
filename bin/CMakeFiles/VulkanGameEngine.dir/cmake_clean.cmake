517e6eda9b38e0907ff14aa3c00632c5b3f0d4f0
JohnEsleyer <ralphesleyer@gmail.com>
2023-09-09 16:05:18 +0800

    Opening a window

---
+++ bin/CMakeFiles/VulkanGameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanGameEngine.dir/src/first_app.cpp.o"
+  "CMakeFiles/VulkanGameEngine.dir/src/first_app.cpp.o.d"
+  "CMakeFiles/VulkanGameEngine.dir/src/lve_window.cpp.o"
+  "CMakeFiles/VulkanGameEngine.dir/src/lve_window.cpp.o.d"
+  "CMakeFiles/VulkanGameEngine.dir/src/main.cpp.o"
+  "CMakeFiles/VulkanGameEngine.dir/src/main.cpp.o.d"
+  "VulkanGameEngine"
+  "VulkanGameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanGameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
