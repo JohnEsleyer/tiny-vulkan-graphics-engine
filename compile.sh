47267c217fcd50f7e93a14b167acf6788c2202cc
JohnEsleyer <ralphesleyer@gmail.com>
2023-09-10 13:38:30 +0800

    added simple shader and vertex shader

---
+++ compile.sh
@@ -0,0 +1,3 @@
+
+/home/john/Applications/vulkan/1.3.261.1/x86_64/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/home/john/Applications/vulkan/1.3.261.1/x86_64/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
